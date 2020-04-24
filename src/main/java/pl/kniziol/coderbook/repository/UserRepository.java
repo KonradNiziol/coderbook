452cd69b79c0fb3d50a69a0b5f11cbc70b2d1bdc
Konrad Niziol <niziol.konrad89@gmail.com>
2020-04-24 15:46:00 +0200

    create user repository

---
+++ src/main/java/pl/kniziol/coderbook/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.kniziol.coderbook.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kniziol.coderbook.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
