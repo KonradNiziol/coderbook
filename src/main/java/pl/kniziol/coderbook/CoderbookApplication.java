ddad9f983dbf426eb25c1d60ee1e4a3743dd0523
Konrad Niziol <niziol.konrad89@gmail.com>
2020-04-24 09:04:00 +0200

    Initial commit with empty project

---
+++ src/main/java/pl/kniziol/coderbook/CoderbookApplication.java
@@ -0,0 +1,13 @@
+package pl.kniziol.coderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kniziol/coderbook/CoderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kniziol.coderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
