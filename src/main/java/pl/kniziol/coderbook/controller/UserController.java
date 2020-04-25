5c3be607f875c24b402e249a1d38ad8f7ba2c51a
Konrad Niziol <niziol.konrad89@gmail.com>
2020-04-25 20:12:17 +0200

    login and register pages and additional get and post mapping to controller

--- src/main/java/pl/kniziol/coderbook/controller/UserController.java
+++ src/main/java/pl/kniziol/coderbook/controller/UserController.java
@@ -1,10 +1,12 @@
 package pl.kniziol.coderbook.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import pl.kniziol.coderbook.dto.LoginCredentials;
 import pl.kniziol.coderbook.dto.UserRegistrationDto;
 import pl.kniziol.coderbook.service.UserService;
 
@@ -12,30 +14,61 @@ import javax.validation.Valid;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @RequestMapping(value = "users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(value = "register")
-    public UserRegistrationDto getUserRegistration(){
-        return new UserRegistrationDto();
+    @GetMapping(value = "/register")
+    public String getUserRegistration(Model model){
+        model.addAttribute("error", "");
+        model.addAttribute("user", new UserRegistrationDto());
+        return "users/register";
     }
 
-    @PostMapping(value = "register")
-    public Map<String, String> registerUser(@Valid UserRegistrationDto userRegistrationDto, BindingResult bindingResult){
-        Map<String, String> errors = bindingResult
+    @PostMapping(value = "/register")
+    public String registerUser(@Valid @ModelAttribute UserRegistrationDto userRegistrationDto, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            var errors = bindingResult
                     .getFieldErrors()
                     .stream()
                     .collect(Collectors.toMap(FieldError::getField, FieldError::getCode));
 
-        if (errors.isEmpty()){
-            userService.registerUser(userRegistrationDto);
+            model.addAttribute("user", userRegistrationDto);
+            model.addAttribute("errors", errors);
+            return "users/register";
         }
-        return errors;
+        userService.registerUser(userRegistrationDto);
+        return "redirect:/users/login";
     }
 
+
+    @GetMapping("/login")
+    public String loginPage(Model model){
+        model.addAttribute("error", "");
+        model.addAttribute("credentials", new LoginCredentials());
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@Valid @ModelAttribute LoginCredentials loginCredentials,  Model model, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            var errors = getErrorsFrom(bindingResult);
+            model.addAttribute("credentials", loginCredentials);
+            model.addAttribute("error", errors);
+            return "users/login";
+        }
+
+        return "index";
+    }
+
+    private Map<String, String> getErrorsFrom(BindingResult bindingResult){
+        return bindingResult
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getCode));
+    }
 
 }
