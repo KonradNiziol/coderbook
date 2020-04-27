b0be9bc287881ca15943016c3a890a679891d7cf
Konrad Niziol <niziol.konrad89@gmail.com>
2020-04-27 11:41:33 +0200

    Add Dependency for spring starter mail and add import to UserService

--- src/main/java/pl/kniziol/coderbook/service/UserService.java
+++ src/main/java/pl/kniziol/coderbook/service/UserService.java
@@ -1,12 +1,13 @@
 package pl.kniziol.coderbook.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.kniziol.coderbook.dto.UserRegistrationDto;
 import pl.kniziol.coderbook.exception.AppException;
 import pl.kniziol.coderbook.maper.Mappers;
-import pl.kniziol.coderbook.model.User;
+import pl.kniziol.coderbook.repository.TokenRepository;
 import pl.kniziol.coderbook.repository.UserRepository;
 
 @Service
@@ -15,6 +16,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenRepository tokenRepository;
+    private final JavaMailSender javaMailSender;
 
     public boolean registerUser(UserRegistrationDto userRegistrationDto){
         if (userRegistrationDto == null){
@@ -22,7 +25,6 @@ public class UserService {
         }
         var user = Mappers.fromRegisterUserToUser(userRegistrationDto);
         user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
-        user.setEnabled(true);
 
         userRepository.save(user);
         return true;
